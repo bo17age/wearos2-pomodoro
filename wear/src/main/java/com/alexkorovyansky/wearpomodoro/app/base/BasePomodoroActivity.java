f6c03de565c9e0a7aead58a7d3ce4afb45e03108
Luong Xuan Linh <linhlx1@vinsmart.net.vn>
2020-12-03 18:18:24 +0700

    convert watchtub to new layout

--- wear/src/main/java/com/alexkorovyansky/wearpomodoro/app/base/BasePomodoroActivity.java
+++ wear/src/main/java/com/alexkorovyansky/wearpomodoro/app/base/BasePomodoroActivity.java
@@ -6,76 +6,20 @@ package com.alexkorovyansky.wearpomodoro.app.base;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.wearable.view.WatchViewStub;
+
+import com.alexkorovyansky.wearpomodoro.R;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import hugo.weaving.DebugLog;
 
-public class BasePomodoroActivity extends Activity implements WatchViewStub.OnLayoutInflatedListener {
+public class BasePomodoroActivity extends Activity {
     private Unbinder unbinder;
 
     @DebugLog
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    @DebugLog
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-    }
-
-    @DebugLog
-    public void setContentViews(int rectRoundLayoutResId) {
-        setContentViews(rectRoundLayoutResId, rectRoundLayoutResId);
-    }
-
-    @DebugLog
-    public void setContentViews(int rectLayoutResId, int roundLayoutResId) {
-        WatchViewStub watchViewStub = new WatchViewStub(this);
-        watchViewStub.setLayoutParams(new WatchViewStub.LayoutParams(WatchViewStub.LayoutParams.MATCH_PARENT, WatchViewStub.LayoutParams.MATCH_PARENT));
-        watchViewStub.setRectLayout(rectLayoutResId);
-        watchViewStub.setRoundLayout(roundLayoutResId);
-        watchViewStub.setOnLayoutInflatedListener(this);
-        setContentView(watchViewStub);
-    }
-
-    @DebugLog
-    @Override
-    public void onLayoutInflated(WatchViewStub stub) {
-        unbinder = ButterKnife.bind(this, stub.getRootView());
-    }
-
-    @DebugLog
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    @DebugLog
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @DebugLog
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @DebugLog
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @DebugLog
-    @Override
-    protected void onDestroy() {
-        unbinder.unbind();
-        super.onDestroy();
+        setContentView(R.layout.activity_main);
     }
 }
